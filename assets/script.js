37e839971b57e1ec72ba2861315dce99f28d3f2a
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-12-09 23:22:50 +0100

    Add js and other logic

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
+new Vue({
+  el: '#app',
+  data: {
+    heading: 'hello',
+    showForm: false,
+    showField: false
+  },
+})
 
+const form = document.querySelector('form');
+const label = document.querySelectorAll('label')
+// submit = () => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    let formLabels = [];
+    let formValues = [];
+    label.forEach(element => {
+      formLabels.push(element.innerHTML);
+    });
+    for (const iterator of data.values()) {
+      formValues.push(iterator);
+    }
+    console.log(...formLabels, ...formValues);
+  })
+// }
+
+const span = document.getElementsByClassName('form-field')
+prev = () => {
+  console.log('Boo')
+  for (let i = 0; i < span.length; i++) {
+    const element = span[i];
+    console.log(element);
+  }
+}
