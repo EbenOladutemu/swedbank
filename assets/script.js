e767c32cce32149b9d3fb41f42b979a1fd218f53
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-12-10 10:35:40 +0100

    Add error messages. Show one field per view

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,67 @@
+
 new Vue({
   el: '#app',
   data: {
     heading: 'hello',
     showForm: false,
-    showField: false
+    showField: false,
+    disabledPrev: true,
+    disabledNext: true,
+    fields: [],
+    fieldIndex: 0,
+    liveTabShow: 0,
+    form: {
+      status: '',
+      employer: '',
+      dob: '',
+      reason: '',
+      amount: '',
+      duration: ''
+    },
+    status: {
+      employed: false,
+      self: false
+    },
+    errorMessages: {
+      employer: false,
+      dob: false,
+      reason: false,
+      amount: false,
+      duration: false
+    }
   },
+  methods: {
+    
+  },
+  updated() {
+    if (this.form.status !== '') {
+      this.disabledNext = false
+    }
+    if (this.form.status === 'employed') {
+      this.status.employed = true
+    } else if (this.form.status === 'self') {
+      this.status.self = true;
+    }
+  },
+  created() {
+  }
 })
+
 
 const form = document.querySelector('form');
 const label = document.querySelectorAll('label')
-// submit = () => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
-    let formLabels = [];
-    let formValues = [];
-    label.forEach(element => {
-      formLabels.push(element.innerHTML);
-    });
-    for (const iterator of data.values()) {
-      formValues.push(iterator);
-    }
-    console.log(...formLabels, ...formValues);
-  })
-// }
+let formLabels = [];
+let formValues = [];
 
-const span = document.getElementsByClassName('form-field')
-prev = () => {
-  console.log('Boo')
-  for (let i = 0; i < span.length; i++) {
-    const element = span[i];
-    console.log(element);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  label.forEach(element => {
+    formLabels.push(element.innerHTML);
+  });
+  for (const iterator of data.values()) {
+    formValues.push(iterator);
   }
-}
+  console.log(...formLabels, ...formValues);
+})
+
